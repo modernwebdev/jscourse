dc5f117ae6af6c9103f067fdc937a8659ddabd5a
Natali <526282@list.ru>
2018-03-26 10:32:18 +0300

    правка в дз к 3ему уроку

--- lesson3/js/main.js
+++ lesson3/js/main.js
@@ -32,11 +32,11 @@ function chooseGoods() {
 	for (let i = 0; i < 3; i++) {
 		let a = prompt('Какой тип товаров будем продавать?');
 
-		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.lenght < 50 ) {
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
 			console.log('Все верно');
 			mainList.shopGoods[i] = a;
 		} else {
-
+			i = i - 1;
 		}
 	}
 }
